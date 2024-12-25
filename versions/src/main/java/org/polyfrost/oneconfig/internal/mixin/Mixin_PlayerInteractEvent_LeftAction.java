26af93d9708765f15b2a71687d64d3919ebb7ead
Deftu <me@deftu.dev>
2024-12-25 16:57:22 +0200

    Implement PlayerInteractEvent for 1.8.9

---
+++ versions/src/main/java/org/polyfrost/oneconfig/internal/mixin/Mixin_PlayerInteractEvent_LeftAction.java
@@ -0,0 +1,56 @@
+package org.polyfrost.oneconfig.internal.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.MovingObjectPosition;
+import org.polyfrost.oneconfig.api.event.v1.EventManager;
+import org.polyfrost.oneconfig.api.event.v1.events.PlayerInteractEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class Mixin_PlayerInteractEvent_LeftAction {
+
+    @Shadow public MovingObjectPosition objectMouseOver;
+    @Shadow public EntityPlayerSP thePlayer;
+
+    @Unique private PlayerInteractEvent lastAttackEvent;
+
+    @Inject(method = "clickMouse", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;swingItem()V", shift = At.Shift.BEFORE), cancellable = true)
+    private void onPlayerAttackCallback(CallbackInfo ci) {
+        MovingObjectPosition rayCastedObject = this.objectMouseOver;
+        PlayerInteractEvent.Type type = PlayerInteractEvent.Type.AIR;
+        if (rayCastedObject != null) {
+            switch (rayCastedObject.typeOfHit) {
+                case BLOCK:
+                    type = PlayerInteractEvent.Type.BLOCK;
+                    break;
+                case ENTITY:
+                    type = PlayerInteractEvent.Type.ENTITY;
+                    break;
+            }
+        }
+
+        lastAttackEvent = new PlayerInteractEvent(this.thePlayer, PlayerInteractEvent.Action.LEFT, type);
+        EventManager.INSTANCE.post(lastAttackEvent);
+        if (lastAttackEvent.cancelled) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "sendClickBlockToController", at = @At("HEAD"), cancellable = true)
+    private void onBlockClickPacketCallback(boolean leftClick, CallbackInfo ci) {
+        if (lastAttackEvent != null && lastAttackEvent.getAction() == PlayerInteractEvent.Action.LEFT && lastAttackEvent.cancelled) {
+            ci.cancel();
+        }
+
+        if (!leftClick) {
+            lastAttackEvent = null;
+        }
+    }
+
+}
