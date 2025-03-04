a947c1e89427a532fb79c94c7dd567e8cc9b54ae
Deftu <me@deftu.dev>
2025-03-04 04:33:11 +0200

    Fix location and compilation of window focus lost event

--- minecraft/versions/1.16.5-forge/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_ModernWindowFocusEvent.java
+++ minecraft/versions/1.16.5-forge/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_ModernWindowFocusEvent.java
@@ -8,10 +8,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Mixin(Minecraft.class)
 public class Mixin_ModernWindowFocusEvent {
-    @Inject(method = "setGameFocused", at = @At("HEAD"))
+    @Inject(method = "setWindowActive", at = @At("HEAD"))
     private void onGameFocused(boolean focused, CallbackInfo ci) {
         if (focused) EventManager.INSTANCE.post(WindowFocusEvent.Gained.INSTANCE);
         else EventManager.INSTANCE.post(WindowFocusEvent.Lost.INSTANCE);
