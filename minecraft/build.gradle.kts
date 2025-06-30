d1f7e95dd6411949c1939b89f235eda1a364112f
nextdayy <79922345+nextdayy@users.noreply.github.com>
2025-06-30 15:17:01 +0100

    update to new polyui

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -131,6 +131,18 @@ dependencies {
     }
 
     api("dev.deftu:enhancedeventbus:2.0.0") // TODO
+
+    if (mcData.isFabric) {
+            modImplementation("net.fabricmc:fabric-language-kotlin:${mcData.dependencies.fabric.fabricLanguageKotlinVersion}")
+
+        if (mcData.isLegacyFabric) {
+            // 1.8.9 - 1.13
+            modImplementation("net.legacyfabric.legacy-fabric-api:legacy-fabric-api:${mcData.dependencies.legacyFabric.legacyFabricApiVersion}")
+        } else {
+            // 1.16.5+
+            modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.dependencies.fabric.fabricApiVersion}")
+        }
+    }
 }
 
 fun DependencyHandlerScope.handleApiDep(dependency: String, isMod: Boolean = false) {
