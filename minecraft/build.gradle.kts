aa797a2df8d7e33a52521e58cf9c8427368a794f
Deftu <me@deftu.dev>
2025-08-10 22:10:27 +0200

    Bruh

--- minecraft/build.gradle.kts
+++ minecraft/build.gradle.kts
@@ -140,8 +140,10 @@ fun DependencyHandlerScope.handleApiDep(dependency: ExternalModuleDependency, is
 
 preprocess {
     val filter: Predicate<File> = Predicate {
-        val seperator = File.pathSeparator
-        !it.toPath().absolutePathString().contains("build${seperator}generated${seperator}ksp")
+        val seperator = File.separator
+        val path = "build${seperator}generated${seperator}ksp"
+        logger.lifecycle("Filtering out $it from preprocessing: $path")
+        !it.toPath().absolutePathString().contains(path)
     }
 
     javaFilter = filter
